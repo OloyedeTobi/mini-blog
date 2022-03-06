e284b398e2f9bfef862ae2931dd6f5b75cc01be0
Oloyede Oluwatobi <oloyedeoluwatobi52@gmail.com>
2022-03-06 22:31:06 +0100

    Added social media links

--- src/blog/Footer.js
+++ src/blog/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
         <footer className="footer-con">
          <div>Contact me:</div>
          <div className="social-media">
-           <div><a><i className="fab fa-twitter"></i></a></div>
-           <div><a><i className="fab fa-instagram"></i></a></div>
-           <div><a><i className="fab fa-linkedin"></i></a></div>
-           <div><a><i className="fab fa-github"></i></a></div>
-           <div><a><i className="fas fa-envelope-square"></i></a></div>
+           <div><a href="https://twitter.com/folaiceyy"><i className="fab fa-twitter"></i></a></div>
+           <div><a href="https://www.instagram.com/oloyede_fola/"><i className="fab fa-instagram"></i></a></div>
+           <div><a href="www.linkedin.com/in/oluwatobi-oloyede-5594a01a3"><i className="fab fa-linkedin"></i></a></div>
+           <div><a href="https://github.com/OloyedeTobi"><i className="fab fa-github"></i></a></div>
+           <div><a href="oloyedetobi@gmail.com"><i className="fas fa-envelope-square"></i></a></div>
           </div>
         </footer>
     </div>
